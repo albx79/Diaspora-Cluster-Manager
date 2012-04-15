4f54ecfab73babf802d6355ef04779cf36040764
Alberto <albx79-at-gmail-dot-com>
2012-04-15 11:46:31 +0200

    roboguice works!!!

--- src/it/albx79/diaspora/activities/SystemViewer.java
+++ src/it/albx79/diaspora/activities/SystemViewer.java
@@ -1,14 +1,16 @@
 package it.albx79.diaspora.activities;
 
 import it.albx79.diaspora.R;
-import it.albx79.diaspora.controllers.Connector;
 import it.albx79.diaspora.models.system.SystemModel;
 import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class SystemViewer extends RoboActivity {
+	@InjectView(R.id.systemName) TextView systemName;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,8 +20,8 @@ public class SystemViewer extends RoboActivity {
 		if (i.getExtras() != null) {
 			model = (SystemModel)i.getExtras().get(SystemModel.KEY);
 		}
-		
-		Connector.bind(model.name, (TextView)findViewById(R.id.systemName));
+		systemName.setText("TODO deserialise model");
+//		Connector.bind(model.name, systemName);
 	}
 
 }
